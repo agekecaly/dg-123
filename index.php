9226debcafab4e475263dc62f1d467728b4bf357
Calvince Ageke <calvince.ageke@bdo-ea.com>
2023-11-21 16:05:45 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+            require 'header.php';
+           ?>
+           <div id="bannerImage">
+               <div class="container">
+                   <center>
+                   <div id="bannerContent">
+                       <h1>We sell .</h1>
+                       <p>Flat 40% OFF on all premium brands.</p>
+                       <a href="products.php" class="btn btn-danger">Shop Now</a>
+                   </div>
+                   </center>
+               </div>
+           </div>
+           <div class="container">
+               <div class="row">
+                   <div class="col-xs-4">
+                       <div  class="thumbnail">
+                           <a href="products.php">
+                                <img src="img/restaurant-interior.jpg" alt="restaurant-interior">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                        <p id="autoResize">restaurant-interior</p>
+                                        <p>Choose among the best available in the world.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/wine-glass.jpg" alt="wine-glass">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                    <p id="autoResize">wine-glass</p>
+                                    <p>Original wine from the best brands.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/top-view-pakistani-food-arrangement.jpg" alt="pakistani-food">
+                           </a>
+                           <center>
+                               <div class="caption">
+                                   <p id="autoResize">pakistani-food</p>
+                                   <p>Our exquisite collection of parkistan foods.</p>
+                               </div>
+                           </center>
+                       </div>
+                   </div>
+               </div>
+           </div>
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center> 
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
